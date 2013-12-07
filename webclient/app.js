d40bdc3efdb68b044d3cee42c8bcb1ce21ffb272
Patrick Negri <patrick@iugu.com>
2013-12-07 16:53:40 -0200

    [ ! ] First steps with Boostrap and Express (WebApp)

--- webclient/app.js
+++ webclient/app.js
@@ -3,10 +3,15 @@ var logfmt = require("logfmt");
 var app = express();
 
 app.use(logfmt.requestLogger());
-app.get('/', function(req,res) {
-  res.send('Hello World!!!');
+app.use(express.static(__dirname + "/public"));
 
+// Algumas rotas (Marketing)
+// Algumas rotas (App - Single Page App)
+app.get('/', function(req,res) {
+  res.render('index');
 });
+
+// Rotas de API
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
